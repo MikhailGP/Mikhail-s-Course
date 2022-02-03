fc4be62616c3a671d0d0c5dbfafc24c7e46bf402
Mikhail <94916928+MikhailGP@users.noreply.github.com>
2022-02-03 22:29:31 +0500

    Fail changed
    Code has been changed

--- Lession 12.7.py
+++ Lession 12.7.py
@@ -4,17 +4,35 @@ TKB = per_cent['ТКБ']
 CKB = per_cent['СКБ']
 VTB = per_cent['ВТБ']
 CBER = per_cent['СБЕР']
+#Наверняка это можно было сделать как-то иначе, проще. Пока не придумал как
 
 money = float(input("Введите планируемую сумму вклада - "))
 
-deposit = [money * TKB * 0.01, money * CKB * 0.01, money * VTB * 0.01, money * CBER * 0.01]
+deposit_TKB = money * TKB * 0.01
+deposit_CKB = money * CKB * 0.01
+deposit_VTB = money * VTB * 0.01
+deposit_CBER = money * CBER * 0.01
+#Наверняка это можно было сделать как-то иначе, проще. Пока не придумал как
 
-max_deposit = max(deposit)
+deposit_all = [deposit_TKB, deposit_CKB, deposit_VTB, deposit_CBER]
 
-deposit = str(deposit)
+max_deposit = max(deposit_all)
 
+deposit_all = str(deposit_all)
 max_deposit = str(max_deposit)
+deposit_TKB = str(deposit_TKB)
+deposit_CKB = str(deposit_CKB)
+deposit_VTB = str(deposit_VTB)
+deposit_CBER = str(deposit_CBER)
+#Наверняка это можно сделать одной командой, пока не понял как
 
-print("Накопленные средства за год в каждом из банков: " + deposit)
+print("Накопленные средства за год в каждом из банков: " + deposit_all)
+#Вывод результата одним списком
 
-print("Максимальная сумма которую вы можете заработать - " + max_deposit + " рублей")
+print("Накопленные средства за год в банке ТКБ: " + deposit_TKB)
+print("Накопленные средства за год в банке СКБ: " + deposit_CKB)
+print("Накопленные средства за год в банке ВТБ: " + deposit_VTB)
+print("Накопленные средства за год в банке СБЕР: " + deposit_CBER)
+#Вывод результата отдельно для каждого из банков
+
+print("Максимальная сумма которую вы можете заработать - " + max_deposit)
